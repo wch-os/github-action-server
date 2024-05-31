1f1c096fa1c5b369cc9dbc446e859afa072004e8
wch-os <wch1224.s@gmail.com>
2024-05-31 19:32:54 +0900

    first commit

---
+++ src/main/java/com/example/github_actions_server/GithubActionsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsServerApplication.class, args);
+	}
+
+}
