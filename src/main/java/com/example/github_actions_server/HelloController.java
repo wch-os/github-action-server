6b6d37dc46d870988bda7c2d5f91ed666d39a23e
wch-os <wch1224.s@gmail.com>
2024-05-31 20:05:57 +0900

    Hello, World!

---
+++ src/main/java/com/example/github_actions_server/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello, World!!";
+    }
+}
